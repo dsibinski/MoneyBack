1647b93bf852f14fc6429d5ad2d94b3d5e2dde07
iks111 <dawidsibinski@gmail.com>
2017-11-06 21:09:06 +0100

    Android: test added for an event with multiple participants

---
+++ Android/MoneyBack.Core/App.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MoneyBack.Core.Utility;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Localization;
+using MvvmCross.Platform;
+using MvvmCross.Platform.IoC;
+
+namespace MoneyBack.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            CreatableTypes()
+                .EndingWith("Repository")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            Mvx.RegisterSingleton<IMvxTextProvider>
+                (new ResxTextProvider(Strings.ResourceManager));
+
+            RegisterAppStart(new AppStart());
+        }
+    }
+}
---
+++ Android/MoneyBack.Core/Utility/ResxTextProvider.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Text;
+using MvvmCross.Localization;
+
+namespace MoneyBack.Core.Utility
+{
+    public class ResxTextProvider : IMvxTextProvider
+    {
+        private readonly ResourceManager _resourceManager;
+
+        public ResxTextProvider(ResourceManager resourceManager)
+        {
+            _resourceManager = resourceManager;
+            CurrentLanguage = new CultureInfo("en-US");
+        }
+
+        public CultureInfo CurrentLanguage { get; set; }
+
+        public string GetText(string namespaceKey,
+            string typeKey, string name)
+        {
+            string resolvedKey = name;
+
+            if (!string.IsNullOrEmpty(typeKey))
+            {
+                resolvedKey = $"{typeKey}.{resolvedKey}";
+            }
+
+            if (!string.IsNullOrEmpty(namespaceKey))
+            {
+                resolvedKey = $"{namespaceKey}.{resolvedKey}";
+            }
+
+            return _resourceManager.GetString(resolvedKey, CurrentLanguage);
+        }
+
+        public string GetText(string namespaceKey, string typeKey, string name, params object[] formatArgs)
+        {
+            string baseText = GetText(namespaceKey, typeKey, name);
+
+            if (string.IsNullOrEmpty(baseText))
+            {
+                return baseText;
+            }
+
+            return string.Format(baseText, formatArgs);
+        }
+
+        public bool TryGetText(out string textValue, string namespaceKey, string typeKey, string name)
+        {
+            try
+            {
+                textValue = GetText(namespaceKey, typeKey, name);
+            }
+            catch (Exception e)
+            {
+                textValue = null;
+                Debug.Write(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool TryGetText(out string textValue, string namespaceKey, string typeKey, string name, params object[] formatArgs)
+        {
+            try
+            {
+                textValue = GetText(namespaceKey, typeKey, name, formatArgs);
+            }
+            catch (Exception e)
+            {
+                textValue = null;
+                Debug.Write(e);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
